9fa936c96a99aa96d267fdf40cc1e6c5c8f23958
unknown <malinduruwantha@gmail.com>
2019-02-22 21:55:28 +0530

    initialiseng luckvin auto care

---
+++ src/app/app.routing.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { CommonModule, } from '@angular/common';
+import { BrowserModule  } from '@angular/platform-browser';
+import { Routes, RouterModule } from '@angular/router';
+
+import { HomeComponent } from './home/home.component';
+import { ProfileComponent } from './profile/profile.component';
+import { SignupComponent } from './signup/signup.component';
+import { LandingComponent } from './landing/landing.component';
+
+import { NucleoiconsComponent } from './components/nucleoicons/nucleoicons.component';
+import { HomepageComponent } from './homepage/homepage.component';
+
+
+const routes: Routes =[
+    { path: 'home',             component: HomeComponent },
+    { path: 'homepage',         component: HomepageComponent },
+    
+    { path: 'user-profile',     component: ProfileComponent },
+    { path: 'signup',           component: SignupComponent },
+    { path: 'landing',          component: LandingComponent },
+    { path: 'nucleoicons',      component: NucleoiconsComponent },
+    { path: '', redirectTo: 'home', pathMatch: 'full' }
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    BrowserModule,
+    RouterModule.forRoot(routes)
+  ],
+  exports: [
+  ],
+})
+export class AppRoutingModule { }
---
+++ src/app/homepage/homepage.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-homepage',
+    templateUrl: './homepage.component.html',
+    styleUrls: ['./homepage.component.scss']
+})
+
+export class HomepageComponent implements OnInit {
+    model = {
+        left: true,
+        middle: false,
+        right: false
+    };
+
+    focus;
+    focus1;
+    constructor() { }
+
+    ngOnInit() {}
+}
+
